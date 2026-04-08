f9f7a55c6681db0d96658c580af1cefb4a854748
whiezzer <endyturrin@gmail.com>
2026-04-08 10:10:17 +0200

    (Add) tall grass
    Added tall grass that has a chance to trigger a fight when the player moves into it.

---
+++ Script/herbe.gd
@@ -0,0 +1,28 @@
+extends Area3D
+
+@export_category("Paramètre des Hautes herbes")
+
+# Probabilité de rencontrer un pokémon dans les hautes herbes en pourcentage
+@export var probabiliteDeRencontre = 12.5
+
+# Fonction appellé au lancement du jeu
+func _ready() -> void:
+	_maxPourcentage()
+	_minPourcentage()
+	randomize()
+
+# Fonction appellé quand le joueur se déplace dans les hautes herbes
+func _onCollision(body) -> void:
+	var aleatoir = randf() * 100
+	if aleatoir <= probabiliteDeRencontre:
+		print("Lancement d'un combat")
+
+# Fonction qui permet de ne pas dépasser le pourcentage de probabilité de rencontre
+func _maxPourcentage() -> void:
+	if probabiliteDeRencontre > 100.0:
+		probabiliteDeRencontre = 100.0
+
+# Fonction qui permet de ne pas avoir une probabilité de rencontre négatif
+func _minPourcentage() -> void:
+	if probabiliteDeRencontre < 0.0:
+		probabiliteDeRencontre = 0.0
--- Script/joueur.gd
+++ Script/joueur.gd
@@ -2,10 +2,10 @@ extends CharacterBody3D
 
 @export_category("Paramètre du joueur")
 
-# Modifier la valeur pour changer la vitesse du joueur
+# Modifier la valeur pour changer la vitesse du joueur (4.0 celle de base)
 @export var vitesse = 4.0
 
-# Modifier la valeur pour changer la vitesse du joueur
+# Modifier la valeur pour changer la position de départ du joueur ((0, 1, 0) celle de base(
 @export var position_initial = Vector3(0, 1, 0)
 
 # Paramètre à ne pas toucher
@@ -14,16 +14,20 @@ var pourcentageDeMouvementJusquALaProchaineTuile = 0.0
 var direction = Vector3.ZERO
 var estEnMouvement = false
 
+# Fonction appellé au lancement du jeu
+func _ready() -> void:
+	position = position_initial
 
+# Fonction appellé à chaque frame
 func _physics_process(delta: float) -> void:
 	if !estEnMouvement:
 		_enAttenteDeCommande()
 	else:
-		_move(delta)
+		_movement(delta)
 
-
+# Fonction qui gère si le joueur doit se déplacer ou pas,
+# en fonction de si oui ou non des commandes de déplacement ont été pressé.
 func _enAttenteDeCommande() -> void:
-
 	if direction.z == 0:
 		direction.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 
@@ -34,9 +38,8 @@ func _enAttenteDeCommande() -> void:
 		position_initial = position
 		estEnMouvement = true
 
-
-func _move(delta: float) -> void:
-
+# Fonction qui permet de déplacer le joueur
+func _movement(delta: float) -> void:
 	pourcentageDeMouvementJusquALaProchaineTuile += vitesse * delta
 
 	if pourcentageDeMouvementJusquALaProchaineTuile >= 1.0:
