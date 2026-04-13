8cb2e6ba3be2eb289215ff906ee8b384c3590ec4
whiezzer <endyturrin@gmail.com>
2026-04-13 10:53:50 +0200

    (Add) Battle interface
    Added a user interface for combat, not yet functional

--- Script/Combat/combatManager.gd
+++ Script/Combat/combatManager.gd
@@ -1,8 +1,26 @@
 extends Node
 class_name Combat
 
+# # Fonction appellé au lancement du combat
+func _ready() -> void:
+	get_parent().get_node("InterfaceCombat/InterfaceInfoPokemon2/Texte_Nom").text = dataDuJeu.pokemonEnnemiStats.nom
+	get_parent().get_node("InterfaceCombat/InterfaceInfoPokemon1/Texte_Nom").text = dataDuJeu.pokemonJoueurStats.nom
+	
+	get_parent().get_node("InterfaceCombat/InterfaceInfoPokemon2/Texte_Level").text += str(dataDuJeu.pokemonEnnemiStats.lvl)
+	get_parent().get_node("InterfaceCombat/InterfaceInfoPokemon1/Texte_Level").text += str(dataDuJeu.pokemonJoueurStats.lvl)
+
+# Fonction qui met à jour les PV des pokemon dans l'interface
+func _MisAJour_PV():
+	get_parent().get_node("InterfaceCombat/InterfaceInfoPokemon2/PV").value = dataDuJeu.pokemonEnnemiStats.pv_Actuels
+	get_parent().get_node("InterfaceCombat/InterfaceInfoPokemon2/PV").max_value = dataDuJeu.pokemonEnnemiStats.pv
+	
+	get_parent().get_node("InterfaceCombat/InterfaceInfoPokemon1/PV").value = dataDuJeu.pokemonJoueurStats.pv_Actuels
+	get_parent().get_node("InterfaceCombat/InterfaceInfoPokemon1/PV").max_value = dataDuJeu.pokemonJoueurStats.pv
+
 # Fonction qui gère les combats
 func _combat() -> void:
+	
+	_MisAJour_PV()
 	
 	print("début du combat")
 	
@@ -43,4 +61,5 @@ func _tour(pokemon : PokemonData) -> void:
 	print(pokemon.nom + " utilise : " + attaqueUtilsé)
 	await get_tree().create_timer(1.0).timeout
 	print("il inflige " + str(pokemon.attaque) + " dégats à " + pokemon.nom)
+	_MisAJour_PV()
 	await get_tree().create_timer(1.0).timeout
