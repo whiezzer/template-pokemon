4cbe5c3689c5dd2bbbfa96faf92183972d06ae28
whiezzer <endyturrin@gmail.com>
2026-05-20 14:39:49 +0200

    (Add) Menu Pokemon Resume in Game

--- Script/Interface/boutonPokemon.gd
+++ Script/Interface/boutonPokemon.gd
@@ -9,6 +9,7 @@ static var boutonActif : TextureButton = null
 var textStats1 : String = ""
 var textStats2 : String = ""
 var textDescription : String = ""
+var textNature: String = ""
 
 var couleur : Color
 var sprite : Texture2D
@@ -28,47 +29,40 @@ func _physics_process(delta: float) -> void:
 			couleur = Color(1, 1, 1)
 			sprite = pokemon.sprite
 		
-		textStats1 = "Nom : " + pokemon.nom + "\n"
-		textStats1 += "Type : " + pokemon.type.nom + "\n"
-		textStats1 += "PV : " + str(pokemon.pv_Actuels) + "/" + str(pokemon.pv) + "\n"
-		textStats1 += "Atq : " + str(pokemon.attaque) + "\n"
-		textStats1 += "Défense : " + str(pokemon.defense) + "\n"
+		textStats1 = pokemon.nom + "\n" +"\n"
+		textStats1 += "  " + str(pokemon.lvl)
 		
-		textStats2 = "Vitesse : "  + str(pokemon.vitesse) + "\n"
-		textStats2 += "Nature : " + "\n" + pokemon.nature.stat1 + pokemon.nature.modificateur1 + "\n" + pokemon.nature.stat2 + pokemon.nature.modificateur2 + "\n"
+		textStats2 = str(pokemon.attaque) + "\n" + "\n"
+		textStats2 += str(pokemon.defense) + "\n" + "\n"
+		textStats2 += str(pokemon.vitesse) 
+		
+		textNature = pokemon.nature.stat1 + pokemon.nature.modificateur1 + "\n" + pokemon.nature.stat2 + pokemon.nature.modificateur2 + "\n"
 		
 		textDescription = pokemon.description
 
 func _on_mouse_entered() -> void:
 	if pokemon != null and boutonActif == null:
-		get_parent().get_node("TextureRect_Pokemon").texture = sprite
-		get_parent().get_node("Label_Stats1").text = textStats1
-		get_parent().get_node("Label_Stats2").text = textStats2
-		get_parent().get_node("TextureRect_Pokemon").modulate = couleur
-		get_parent().get_node("Label_Description").text = textDescription
+		return
 
 func _on_mouse_exited() -> void:
 	if boutonActif != null:
-		get_parent().get_node("TextureRect_Pokemon").texture = boutonActif.sprite
-		get_parent().get_node("Label_Stats1").text = boutonActif.textStats1
-		get_parent().get_node("Label_Stats2").text = boutonActif.textStats2
-		get_parent().get_node("TextureRect_Pokemon").modulate = boutonActif.couleur
-		get_parent().get_node("Label_Description").text = boutonActif.textDescription
+		return
 	else:
-		get_parent().get_node("TextureRect_Pokemon").texture = null
-		get_parent().get_node("Label_Stats1").text = ""
-		get_parent().get_node("Label_Stats2").text = ""
-		get_parent().get_node("TextureRect_Pokemon").modulate = Color(1, 1, 1)
-		get_parent().get_node("Label_Description").text = ""
+		return
 
 func _on_pressed() -> void:
 	if boutonActif == self:
 		boutonActif = null
+		get_parent().get_node("Resume").visible = false
 	else:
 		if pokemon != null:
 			boutonActif = self
-			get_parent().get_node("TextureRect_Pokemon").texture = boutonActif.sprite
-			get_parent().get_node("Label_Stats1").text = boutonActif.textStats1
-			get_parent().get_node("Label_Stats2").text = boutonActif.textStats2
-			get_parent().get_node("TextureRect_Pokemon").modulate = boutonActif.couleur
-			get_parent().get_node("Label_Description").text = boutonActif.textDescription
+			get_parent().get_node("Resume").visible = true
+			get_parent().get_node("Resume/TextureRect_Pokemon").texture = boutonActif.sprite
+			get_parent().get_node("Resume/Label_Stats1").text = boutonActif.textStats1
+			get_parent().get_node("Resume/Label_Stats2").text = boutonActif.textStats2
+			get_parent().get_node("Resume/TextureRect_Pokemon").modulate = boutonActif.couleur
+			get_parent().get_node("Resume/Label_Description").text = boutonActif.textDescription
+			get_parent().get_node("Resume/TextureProgressBar").max_value = boutonActif.pokemon.pv
+			get_parent().get_node("Resume/TextureProgressBar").value = boutonActif.pokemon.pv_Actuels
+			get_parent().get_node("Resume/Label_Nature").text = boutonActif.textNature
