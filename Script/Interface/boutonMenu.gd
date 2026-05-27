58442bcf349435e44e0122e3a8a74382ca00f6e3
whiezzer <endyturrin@gmail.com>
2026-05-27 21:06:12 +0200

    (Fix) Design main menu

--- Script/Interface/boutonMenu.gd
+++ Script/Interface/boutonMenu.gd
@@ -9,3 +9,12 @@ func _on_menu_pokemon_pressed() -> void:
 	$AudioStreamPlayer2D.playing = true
 	
 	ecran_de_transition._fondu("InterfaceMenu/MenuGodomon")
+
+func _quitter() -> void:
+	
+	get_tree().quit()
+
+func _on_credit_pressed() -> void:
+	$AudioStreamPlayer2D.playing = true
+	
+	ecran_de_transition._fondu("InterfaceMenu/Credit")
